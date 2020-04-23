c74b9a7f188c62365f2667e0a26a6eaf282edfd5
vlad <vladkalyuzhnyu@gmail.com>
2020-04-23 15:09:54 +0300

    adapter

--- app/src/main/java/defpackage/odometer/screen/main/TabsAdapter.kt
+++ app/src/main/java/defpackage/odometer/screen/main/TabsAdapter.kt
@@ -4,16 +4,38 @@ package defpackage.odometer.screen.main
 
 import android.app.Fragment
 import android.app.FragmentManager
-import androidx.legacy.app.FragmentPagerAdapter
+import android.view.View
+import android.view.ViewGroup
+import androidx.collection.SimpleArrayMap
+import androidx.legacy.app.FragmentStatePagerAdapter
 
-class TabsAdapter(manager: FragmentManager) : FragmentPagerAdapter(manager) {
+class TabsAdapter(manager: FragmentManager) : FragmentStatePagerAdapter(manager) {
 
-    override fun getItem(position: Int): Fragment = when (position) {
-        0 -> FirstFragment.newInstance()
-        else -> SecondFragment.newInstance()
+    val fragments = SimpleArrayMap<Int, Fragment>()
+
+    override fun getItem(position: Int): Fragment? {
+        if (!fragments.containsKey(position)) {
+            val fragment = when (position) {
+                0 -> FirstFragment.newInstance()
+                else -> SecondFragment.newInstance()
+            }
+            fragments.put(position, fragment)
+            return fragment
+        }
+        return fragments.get(position)
     }
 
     override fun getCount(): Int = 2
 
     override fun getPageTitle(position: Int): CharSequence? = null
+
+    override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
+        fragments.remove(position)
+        super.destroyItem(container, position, `object`)
+    }
+
+    override fun destroyItem(container: View, position: Int, `object`: Any) {
+        fragments.remove(position)
+        super.destroyItem(container, position, `object`)
+    }
 }
