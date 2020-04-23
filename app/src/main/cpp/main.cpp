9cfa79f68b99e37b159583f86ad5e12001d90c6b
vlad <vladkalyuzhnyu@gmail.com>
2020-04-23 14:03:03 +0300

    main

--- app/src/main/cpp/main.cpp
+++ app/src/main/cpp/main.cpp
@@ -4,10 +4,12 @@
 
 #define TAG "CPP"
 
-#define d(log, ...) if (log) __android_log_print(ANDROID_LOG_DEBUG, TAG, __VA_ARGS__)
-#define i(log, ...) if (log) __android_log_print(ANDROID_LOG_INFO, TAG, __VA_ARGS__)
-#define w(log, ...) if (log) __android_log_print(ANDROID_LOG_WARN, TAG, __VA_ARGS__)
-#define e(log, ...) if (log) __android_log_print(ANDROID_LOG_ERROR, TAG, __VA_ARGS__)
+#define d(...) __android_log_print(ANDROID_LOG_DEBUG, TAG, __VA_ARGS__)
+#define i(...) __android_log_print(ANDROID_LOG_INFO, TAG, __VA_ARGS__)
+#define w(...) __android_log_print(ANDROID_LOG_WARN, TAG, __VA_ARGS__)
+#define e(...) __android_log_print(ANDROID_LOG_ERROR, TAG, __VA_ARGS__)
+
+#define mms2kmh(speed) (int) round(speed * 3600)
 
 /**
  * @param time in millis
@@ -23,22 +25,35 @@ Java_defpackage_odometer_LocationManager_getSpeed(JNIEnv *env, jobject, jboolean
     }
     jlong *x = env->GetLongArrayElements(time, 0);
     jfloat *y = env->GetFloatArrayElements(distances, 0);
+    if (size == 2) {
+        float speed = y[1] / (x[1] - x[0]);
+        env->ReleaseLongArrayElements(time, x, 0);
+        env->ReleaseFloatArrayElements(distances, y, 0);
+        return mms2kmh(speed);
+    }
     float sumX = 0;
     float sumY = 0;
     float sumX2 = 0;
     float sumXY = 0;
     for (int i = 1; i < size; i++) {
         float speed = y[i] / (x[i] - x[i - 1]);
+        if (log) {
+            d("speed[%i] = %f km/h", i - 1, speed * 3600);
+        }
         sumX += x[i];
         sumY += speed;
         sumX2 += x[i] * x[i];
         sumXY += x[i] * speed;
     }
+    size--;
     // y = a * x + b
     float a = (size * sumXY - (sumX * sumY)) / (size * sumX2 - sumX * sumX);
     float b = (sumY - a * sumX) / size;
-    int speed = (int) round((a * x[size - 1] + b) * 3600);
+    if (log) {
+        d("y = %f * x + %f", a, b);
+    }
+    float speed = (a * x[size - 1] + b) * 3600;
     env->ReleaseLongArrayElements(time, x, 0);
     env->ReleaseFloatArrayElements(distances, y, 0);
-    return speed;
+    return mms2kmh(speed);
 }
