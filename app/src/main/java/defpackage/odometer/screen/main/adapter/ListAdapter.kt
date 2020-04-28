91653a57de03ae658e9f8d5a6c21ee7d2211ae20
vlad <vladkalyuzhnyu@gmail.com>
2020-04-28 19:30:30 +0300

    adapter

--- app/src/main/java/defpackage/odometer/screen/main/adapter/ListAdapter.kt
+++ app/src/main/java/defpackage/odometer/screen/main/adapter/ListAdapter.kt
@@ -2,8 +2,10 @@ package defpackage.odometer.screen.main.adapter
 
 import android.annotation.SuppressLint
 import android.text.TextWatcher
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.PopupMenu
 import defpackage.odometer.R
 import defpackage.odometer.extensions.onTextChanged
 import defpackage.odometer.extensions.setTextSelection
@@ -23,7 +25,8 @@ class ListAdapter(listener: ListListener) : BaseAdapter<ListListener, LimitEntit
         (holder as ViewHolder).release()
     }
 
-    inner class ViewHolder(itemView: View) : BaseHolder<LimitEntity>(itemView) {
+    inner class ViewHolder(itemView: View) : BaseHolder<LimitEntity>(itemView),
+        PopupMenu.OnMenuItemClickListener {
 
         private val speed = itemView.et_speed
 
@@ -44,12 +47,28 @@ class ListAdapter(listener: ListListener) : BaseAdapter<ListListener, LimitEntit
 
                 }
             }
+            val popup = PopupMenu(appContext, menu).also {
+                it.menuInflater.inflate(R.menu.popup_menu, it.menu)
+                it.setOnMenuItemClickListener(this)
+            }
+            itemView.im_menu.setOnClickListener {
+                popup.show()
+            }
         }
 
         @SuppressLint("SetTextI18n")
         override fun onBindItem(position: Int, item: LimitEntity) {
             speed.setTextSelection(item.speed.toString())
             distance.setTextSelection(item.distance.toString())
+        }
+
+        override fun onMenuItemClick(item: MenuItem): Boolean {
+            when (item.itemId) {
+                R.id.action_delete -> {
+
+                }
+            }
+            return true
         }
 
         fun release() {
